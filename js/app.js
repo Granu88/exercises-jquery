7e2f40bc91a1c880f3b8d8fb03a1fda15cd4b7ac
David VALLON <davidvallon@hotmail.fr>
2019-10-29 11:00:42 +0100

    Exo 3

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 $(function() {
   // Here your JS code, good luck !
   $('.dropdown-toggle').dropdown();
+
   $(".nav-link").removeClass("disabled");
+
+  $("#search").keyup(function() {
+    var value=$('#search').val();
+  $('span').html(value);
+
+  
+
+    console.log(value)
+  });
 });
