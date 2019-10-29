fcd4a2725d1f4151a2f2382b9fcb8eba66d02c42
David VALLON <davidvallon@hotmail.fr>
2019-10-29 10:02:57 +0100

    Exo 2

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 $(function() {
   // Here your JS code, good luck !
   $('.dropdown-toggle').dropdown();
-  $( "disabled" ).prop( "disabled", true );
-  $("a").removeClass("disabled");
+  $(".nav-link").removeClass("disabled");
 });
